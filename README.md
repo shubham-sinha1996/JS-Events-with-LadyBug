5689b93288dd8b65f6efff0c6f0fa843642ccb43
shubham <ss.guwahati1996@gmail.com>
2024-09-27 18:57:38 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# LadyBug Movement - Event Listener
+
+This project demonstrates how to use JavaScript event listeners to move an element (LadyBug) in four directions: **up**, **down**, **left**, and **right** by clicking on arrow key buttons.
+
+## Features
+
+- **Event Listener**: Listens for `click` events to detect button clicks.
+- **Movement**: Moves the LadyBug element across the screen in the specified direction.
+- **Simple and Clean UI**: Minimal HTML and CSS to focus on the JavaScript logic.
+
+## How It Works
+
+When the user clciks the arrow keys:
+
+- **Up Arrow**: Moves the LadyBug up.
+- **Down Arrow**: Moves the LadyBug down.
+- **Left Arrow**: Moves the LadyBug left.
+- **Right Arrow**: Moves the LadyBug right.
---
+++ src/js/index.js
@@ -0,0 +1,55 @@
+const bug = document.querySelector('.ladybird');
+const upBtn = document.querySelector('#up');
+const downBtn = document.querySelector('#down');
+const rightBtn = document.querySelector('#right');
+const leftBtn = document.querySelector('#left');
+const homeBtn = document.querySelector('#home');
+const turnClockwiseBtn = document.querySelector('#turnClockWise');
+const turnAntiClockwiseBtn = document.querySelector('#turnAntiClockWise');
+
+let xPos = 0, yPos = 0, rotation = 0 ;
+
+const transform = () => {
+    bug.style.left = `${xPos}px`;
+    bug.style.top = `${yPos}px`;
+}
+const turnLeft = () => {
+    xPos > 0 && (xPos -= 10);
+    transform();
+};
+const turnRight = () => {
+    xPos < 450 && (xPos += 10);
+    transform();
+};
+const turnTop = () => {
+    yPos > 0 && (yPos -= 10);
+    transform();
+};
+const turnDown = () => {
+    yPos < 450 && (yPos += 10);
+    transform();
+};
+const home = () => {
+    xPos = 0;
+    yPos = 0;
+    transform();
+}
+const turnClockWise = () => {
+    rotation += 90;
+    bug.style.transform = `rotate(${rotation}deg)`;
+};
+
+const turnAntiClockwise = () => {
+    rotation -= 90;
+    bug.style.transform = `rotate(${rotation}deg)`;
+};
+
+
+rightBtn.addEventListener('click', turnRight);
+leftBtn.addEventListener('click',turnLeft);
+downBtn.addEventListener('click',turnDown);
+upBtn.addEventListener('click',turnTop);
+homeBtn.addEventListener('click', home);
+turnAntiClockwiseBtn.addEventListener('click',turnAntiClockwise);
+turnClockwiseBtn.addEventListener('click',turnClockWise);
+
